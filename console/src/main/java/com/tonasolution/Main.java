e9c355cef91f9f3098380dc5df3e54994322b64a
zouhaire <zouhaire@idside.de>
2019-12-05 17:18:25 +0100

    integrated spring boot

---
+++ console/src/main/java/com/tonasolution/Main.java
@@ -0,0 +1,17 @@
+package com.tonasolution;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@Slf4j
+@SpringBootApplication
+public class Main {
+
+    public static void main(String[] args) {
+        log.info("Guess The number Game");
+
+        SpringApplication.run(Main.class, args);
+    }
+}
