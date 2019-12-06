fcf4c32333b2d5eab6ba6ee6e3c132d1aac92b8e
zouhaire <zouhaire@idside.de>
2019-12-06 16:54:06 +0100

    added thymeleaf package

---
+++ web/src/main/java/com/tonasolution/util/ViewNames.java
@@ -0,0 +1,10 @@
+package com.tonasolution.util;
+
+public class ViewNames {
+
+    // == constants ==
+    public static final String HOME = "home";
+
+    // == constructors ==
+    private ViewNames(){}
+}
