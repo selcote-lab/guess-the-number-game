33d3170c1e0d0082e48e1446986df509195a14af
zouhaire <zouhaire@idside.de>
2019-12-07 11:57:49 +0100

    impl gameService

---
+++ web/src/main/java/com/tonasolution/service/GameService.java
@@ -0,0 +1,14 @@
+package com.tonasolution.service;
+
+public interface GameService {
+
+    boolean isGameOver();
+
+    String getMainMessage();
+
+    String getResultMessage();
+
+    void checkGuess(int guess);
+
+    void reset();
+}
