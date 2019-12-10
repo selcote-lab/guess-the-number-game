04efa82ae3763c7d0f1a30aab6e0157610e78d36
zouhaire <zouhaire@idside.de>
2019-12-10 17:01:05 +0100

    added play thymeleaf template

---
+++ web/src/main/java/com/tonasolution/GameController.java
@@ -0,0 +1,52 @@
+package com.tonasolution;
+
+import com.tonasolution.service.GameService;
+import com.tonasolution.util.AttributeNames;
+import com.tonasolution.util.GameMappings;
+import com.tonasolution.util.ViewNames;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestAttribute;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Slf4j
+@Controller
+public class GameController {
+
+    // == fields ==
+    private final GameService gameService;
+
+    // == constructors ==
+
+    @Autowired
+    public GameController(GameService gameService) {
+        this.gameService = gameService;
+    }
+
+    // == request methods ==
+    @GetMapping(GameMappings.PLAY)
+    public String play(Model model) {
+
+        model.addAttribute(AttributeNames.MAIN_MESSAGE, gameService.getMainMessage());
+        model.addAttribute(AttributeNames.RESULT_MESSAGE, gameService.getResultMessage());
+        log.info("model = {}", model);
+
+        if (gameService.isGameOver()){
+            return ViewNames.GAME_OVER;
+        }
+
+        return ViewNames.PLAY;
+    }
+
+    @PostMapping(GameMappings.PLAY)
+    public String processMessage(@RequestParam int guess) {
+        log.info("guess = {}", guess);
+        gameService.checkGuess(guess);
+
+        return GameMappings.REDIRECT_PLAY;
+    }
+}
---
+++ web/src/main/java/com/tonasolution/util/AttributeNames.java
@@ -0,0 +1,11 @@
+package com.tonasolution.util;
+
+public final class AttributeNames {
+
+    // == constants ==
+    public static final String MAIN_MESSAGE = "mainMessage";
+    public static final String RESULT_MESSAGE = "resultMessage";
+
+    // == constructors ==
+    private AttributeNames(){}
+}
